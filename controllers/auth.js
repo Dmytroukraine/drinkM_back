ecf026ecc184fc7235fdda99177202ffaf72cbe1
Dmytro Kozlovskyi <120205405+Dmytroukraine@users.noreply.github.com>
2023-11-02 22:23:15 +0200

    fix s

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,7 +30,7 @@ const signup = async (req, res) => {
     id: newUser._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
 
   await User.findByIdAndUpdate(newUser._id, { token });
 
@@ -63,7 +63,7 @@ const signin = async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
